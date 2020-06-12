d81a44be9256e9454b2411f5391bfbbb80a065b2
unknown <krychaz@protonmail.com>
2020-06-12 16:20:03 +0100

    created database connection and finished models

--- medshop/store/models.py
+++ medshop/store/models.py
@@ -3,10 +3,8 @@ from django.contrib.auth.models import User
 
 # Create your models here
 
-
 class Customer(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=150, null=True)
     surname = models.CharField(max_length=150, null=True)
     email = models.CharField(max_length=150, null=True)
@@ -15,7 +13,6 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
     name = models.CharField(max_length=150, null=True)
     price = models.FloatField()
@@ -23,13 +20,35 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-
 class Order(models.Model):
-    customer = models.ForeignKey(
-        Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return str(self.id)
+        return self.name
+
+class ItemsOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Shipping(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=150, null=False)
+    city = models.CharField(max_length=150, null=False)
+    state = models.CharField(max_length=150, null=False)
+    zipcode = models.CharField(max_length=150, null=False)
+    date_added = models.CharField(max_length=150, null=False)
+
+    def __str__(self):
+        return self.address
+
+    
+    
+    
+
+    
