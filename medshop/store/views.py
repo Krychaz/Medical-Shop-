98bbbc2cb13e8f00c1242b3336853713d45b1cec
unknown <krychaz@protonmail.com>
2020-08-13 12:45:46 +0100

    more logic fixes

--- medshop/store/views.py
+++ medshop/store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+import datetime
+from .models import * 
 
 
 def store(request):
@@ -74,5 +76,30 @@ def updateItem(request):
 
 
 def processOrder(request):
-    print('Data:', request.body)
-    return JsonResponse('Payments Complete', safe=False)
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	else:
+		customer, order = guestOrder(request, data)
+
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	if total == order.getCartTotal:
+		order.complete = True
+	order.save()
+
+	if order.shipping == True:
+		Shipping.objects.create(
+		customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],
+		)
+
+	return JsonResponse('Payment submitted..', safe=False)
